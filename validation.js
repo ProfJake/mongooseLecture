61b5f2c3964d9286c05f69220fdb3686f279406b
Jake Levy <levyj63@rowan.edu>
2021-03-31 16:27:00 +0000

    Added comments

--- validation.js
+++ validation.js
@@ -1,3 +1,8 @@
+/*validation.js
+An example of Mongoose's simple "requirement" validation
+Jake Levy
+Mar 2021
+*/
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
@@ -13,6 +18,7 @@ const Student = mongoose.model('Student', studentSchema);
 const newStudent = new Student();
 
 newStudent.save((error)=>{
+    //validateSync will check any register validation rules on a Schema
     error = newStudent.validateSync();
     
     console.log(error.message)
